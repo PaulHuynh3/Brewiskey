06c249a006913193174753b1d933f1c548a72d9c
Paul <paul.huynh3@gmail.com>
2018-02-22 23:03:11 -0500

    add resetpasswordvc

---
+++ Brewiskey/Login/ForgotPasswordViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ForgotPasswordViewController.swift
+//  Brewiskey
+//
+//  Created by Paul on 2018-02-22.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+import Foundation
+
+class ForgotPasswordViewController: UIViewController {
+    
+    
+}
