8cfb6bb44e46db6f5510b21f310323bcd1d59dc6
Paul <paul.huynh3@gmail.com>
2018-02-22 23:18:54 -0500

    moved infoplist

--- Brewiskey/Login/ForgotPasswordViewController.swift
+++ Brewiskey/Login/ForgotPasswordViewController.swift
@@ -5,10 +5,47 @@
 //  Created by Paul on 2018-02-22.
 //  Copyright © 2018 Paul. All rights reserved.
 //
-
-import Foundation
+import UIKit
+import Firebase
 
 class ForgotPasswordViewController: UIViewController {
+    
+    @IBOutlet weak var emailImageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var errorCircleImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        errorCircleImageView.isHidden = true
+    }
+    
+    
+    @IBAction func resetPasswordButtonTapped(_ sender: Any) {
+        
+        if emailTextField.text == ""{
+            emailImageView.image = UIImage(named: "RedRectangle")
+            errorCircleImageView.isHidden = false
+            return
+        }
+        guard let email = emailTextField.text else {return}
+        
+        activityIndicator.startAnimating()
+        
+        Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+            
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: "The email is not valid", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                self.performSegue(withIdentifier: "resetPasswordIdentifier", sender: self)
+            }
+            
+        }
+        activityIndicator.stopAnimating()
+    }
     
     
 }
