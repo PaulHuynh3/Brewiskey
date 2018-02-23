2a35eb50c010985cc67e873db73b26c557fb400e
Paul <paul.huynh3@gmail.com>
2018-02-23 16:24:13 -0500

    add data models

---
+++ Brewiskey/Data/DataModel.swift
@@ -0,0 +1,41 @@
+//
+//  User.swift
+//  Brewiskey
+//
+//  Created by Paul on 2018-02-22.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+import Foundation
+import Firebase
+import UIKit
+
+class User: NSObject {
+    var id: String?
+    var username: String?
+    var email: String?
+    var profileImageUrl: String?
+}
+
+class Spirits: NSObject {
+    var name: String?
+    var price: String?
+    var percent: String?
+    var country: String?
+    var shortDescription: String?
+}
+
+class Wine: NSObject {
+    var name: String?
+    var price: String?
+    var percent: String?
+    var country: String?
+    var shortDescription: String?
+}
+
+class Beer: NSObject {
+    var name: String?
+    var price: String?
+    var percent: String?
+    var country: String?
+    var shortDescription: String?
+}
