318134c6a5936412ee25e71fed79494efcd9f1f7
Paul <paul.huynh3@gmail.com>
2018-02-24 17:05:57 -0500

    aset up profile page and test marketplace with temp products

---
+++ Brewiskey/Main/MarketTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MarketTableViewCell.swift
+//  Brewiskey
+//
+//  Created by Paul on 2018-02-24.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+import UIKit
+
+class MarketTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var alcoholImageView: UIImageView!
+    @IBOutlet weak var alcoholNameLabel: UILabel!
+    @IBOutlet weak var alcoholPriceLabel: UILabel!
+        
+}
--- Brewiskey/Main/MarketTableViewController.swift
+++ Brewiskey/Main/MarketTableViewController.swift
@@ -83,9 +83,7 @@ class MarketTableViewController: UITableViewController {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
      }
-     */
-
-    
+     */    
 }
 
 
