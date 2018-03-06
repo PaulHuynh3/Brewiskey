2862065f6f920e47358f3404597147bfa3e594fa
Paul <paul.huynh3@gmail.com>
2018-03-05 21:04:04 -0500

    add container view in dynamic tableviewcell

--- Brewiskey/Main/AlcoholSelectionTableViewController.swift
+++ Brewiskey/Main/AlcoholSelectionTableViewController.swift
@@ -29,7 +29,23 @@ class AlcoholSelectionTableViewController: UITableViewController {
             beerSelection = [firstBeer, secondBeer, (beer?.singleBottlePrice)!, (beer?.sixPackBottlePrice)!]
         }
         //initialize the array
-        selectedItem = []
+        selectedItem = NSMutableArray()
+    }
+    
+    
+    @IBAction func addToCartButtonTapped(_ sender: Any) {
+        if self.navigationController is MarketNavController {
+            let marketNav = self.navigationController as! MarketNavController
+            let user = marketNav.user
+            
+            user?.alcoholItems?.addObjects(from: [selectedItem])
+        }
+        
+    }
+    
+    @IBAction func checkoutButtonTapped(_ sender: Any) {
+        
+        
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
@@ -46,7 +62,6 @@ class AlcoholSelectionTableViewController: UITableViewController {
     }
     
     
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -55,28 +70,31 @@ class AlcoholSelectionTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75
     }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //want to return 5 if its beermode, reutn 3 if its spirits and 1 if its wine
         if beerMode == true {
-            return beerSelection.count
+              return beerSelection.count
         } else {
             return 1
         }
+      
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
         let beer = beerSelection[indexPath.row]
-        
+
         cell.textLabel?.text = beer
-        
+
         if selectedItem.contains(beer){
             cell.accessoryType = .checkmark
         } else {
             cell.accessoryType = .none
         }
         return cell
+        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
