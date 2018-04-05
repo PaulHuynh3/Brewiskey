8a77c9c8b3aee2c15373f82c0b92361a65578322
Paul <paul.huynh3@gmail.com>
2018-04-05 13:59:37 -0400

    onboardingview

---
+++ Brewiskey/Onboarding/BaseOnboardingView.swift
@@ -0,0 +1,14 @@
+//
+//  BaseOnboardingView.swift
+//  Brewiskey
+//
+//  Created by Paul on 2018-04-05.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+import Foundation
+
+class BaseOnboardingView: UIView {
+    
+    
+}
