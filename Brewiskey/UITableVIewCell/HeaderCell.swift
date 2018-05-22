3cfd49eda01e3110274f6ccfc96675c1fd76592d
Paul <paul.huynh3@gmail.com>
2018-05-22 17:57:32 -0400

    add dynamic link helper code with referraltableviewcontroller and its tableview cell files

---
+++ Brewiskey/UITableVIewCell/HeaderCell.swift
@@ -0,0 +1,9 @@
+//
+//  HeaderCell.swift
+//  Brewiskey
+//
+//  Created by Paul on 2018-05-21.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+import Foundation
---
+++ Brewiskey/UITableVIewCell/ParamTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ParamTableViewCell.swift
+//  Brewiskey
+//
+//  Created by Paul on 2018-05-21.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+import Foundation
