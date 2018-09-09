2ce9c7e40a4646c71bfa751ebd1268e2e7e3c663
paulhuynh <paul.huynh3@gmail.com>
2018-09-09 01:53:02 -0400

    improve onboading swiping

--- Brewiskey/Onboarding/IntroductionScreenViewController.swift
+++ Brewiskey/Onboarding/IntroductionScreenViewController.swift
@@ -19,93 +19,41 @@ protocol BaseOnboardingScreenDelegate{
 class IntroductionScreenViewController: UIViewController {
     
     @IBOutlet weak var onboardingView: UIView!
-    var index = 0
-    var kNumberOfOnboardingScreens = 2
-    var kRequiredNumberToTapForward = CGFloat(0.3)
+    var onboardingViews = [BaseOnboardingView]()
+    @IBOutlet weak var onboardingScrollView: UIScrollView!
     
     override func viewDidLoad() {
-        loadOnboardingScreens()
-        setupTapGesture()
-        setupSwipeGesture()
+        onboardingViews = createOnboardinViews()
+        setupSlideScrollView(onboardingViews)
     }
     
-    private func loadOnboardingScreens(){
-        guard let objects = Bundle.main.loadNibNamed("OnboardingView\(self.index)", owner: nil, options: [:]) else {
-            return
+    fileprivate func createOnboardinViews() -> [BaseOnboardingView] {
+        let onboardingViewOne = "OnboardingView0"
+        let onboardingViewTwo = "OnboardingView1"
+        let onboardingViewThree = "OnboardingView2"
+        
+        let onboardingView0 = Bundle.main.loadNibNamed(onboardingViewOne, owner: nil, options: nil)?.first as! BaseOnboardingView
+        let onboardingView1 = Bundle.main.loadNibNamed(onboardingViewTwo, owner: nil, options: nil)?.first as! BaseOnboardingView
+        let onboardingView2 = Bundle.main.loadNibNamed(onboardingViewThree, owner: nil, options: nil)?.first as! BaseOnboardingView
+        
+        let onboardingViews = [onboardingView0, onboardingView1, onboardingView2]
+        
+        for onboardingView in onboardingViews {
+            onboardingView.navigateUserDelegate = self
         }
-        guard let view = objects[0] as? BaseOnboardingView else {
-            return
-        }
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor.clear
-        view.navigateUserDelegate = self
-        
-        for subview in self.onboardingView.subviews {
-            subview.removeFromSuperview()
-        }
-        self.onboardingView.addSubview(view)
-        
-        let views = [ //necessary to configure the view's constraint's..
-            "onboardingView" : view
-        ]
-        self.onboardingView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[onboardingView]|", options: [], metrics: nil, views: views))
-        self.onboardingView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[onboardingView]|", options: [], metrics: nil, views: views))
-        
-        BrewiskeyAnalytics().onboardingScreen(index: self.index)
+        return onboardingViews
     }
     
-    private func setupTapGesture(){
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
-        self.view.addGestureRecognizer(tapGestureRecognizer)
-    }
-    
-    @objc func tapped(sender: UITapGestureRecognizer){
-        if sender.state == .ended {
-            let tapLocation = sender.location(in: self.view)
-            
-            if tapLocation.x > self.view.frame.size.width * kRequiredNumberToTapForward {
-                guard index < kNumberOfOnboardingScreens else {
-                    return
-                }
-                index += 1
-            } else { //tapped backwards
-                guard index > 0 else {
-                    return
-                }
-                index -= 1
-            }
-            self.loadOnboardingScreens()
+    fileprivate func setupSlideScrollView(_ onboardingViews: [UIView]) {
+        onboardingScrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        onboardingScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(onboardingViews.count), height: view.frame.height)
+        onboardingScrollView.isPagingEnabled = true
+        
+        for i in 0 ..< onboardingViews.count {
+            onboardingViews[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            onboardingScrollView.addSubview(onboardingViews[i])
         }
     }
-    
-    private func setupSwipeGesture(){
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
-        swipeLeft.direction = .left
-        self.view.addGestureRecognizer(swipeLeft)
-        
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
-        swipeRight.direction = .right
-        self.view.addGestureRecognizer(swipeRight)
-    }
-    
-    @objc func handleSwipe(sender: UISwipeGestureRecognizer){
-        if sender.state == .ended {
-            if sender.direction == UISwipeGestureRecognizerDirection.left{
-                guard index < kNumberOfOnboardingScreens else {
-                    return
-                }
-                index += 1
-            } else if sender.direction == UISwipeGestureRecognizerDirection.right{
-                guard index > 0 else {
-                    return
-                }
-                index -= 1
-            }
-            self.loadOnboardingScreens()
-        }
-    }
-    
 }
 
 extension IntroductionScreenViewController {
